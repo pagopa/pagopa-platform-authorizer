4cc550ec51da9ede6d81d4fcae411f22147336a0
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-04-20 15:33:23 +0200

    [PAGOPA-859] fix: updated container entity

--- src/main/java/it/gov/pagopa/authorizer/entity/SubscriptionKeyDomain.java
+++ src/main/java/it/gov/pagopa/authorizer/entity/SubscriptionKeyDomain.java
@@ -1,11 +1,16 @@
 package it.gov.pagopa.authorizer.entity;
 
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+@Container(containerName = "skeydomains")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,13 +18,16 @@ import java.util.List;
 @Builder
 public class SubscriptionKeyDomain implements Serializable {
 
+    @Id
+    @GeneratedValue
     private String id;
 
+    @PartitionKey
     private String domain;
 
     private String subkey;
 
     private List<String> authorization;
 
-    private Date insertedAt;
+    private LocalDate insertedAt;
 }
