836af2fe6b8c13d8375f4de4e9468995c3ecdcde
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-06-23 15:20:02 +0200

    [PAGOPA-996] feat: updated integration tests

--- integration-test/src/step_definitions/support/clients/enrolled_ec_client.js
+++ integration-test/src/step_definitions/support/clients/enrolled_ec_client.js
@@ -6,7 +6,19 @@ const enrolled_ec_host = process.env.enrolled_ec_host;
 const ipAddress = ip.address();
 
 function getEnrolledEC(domain) {
-    const host = `${enrolled_ec_host}/${domain}`;
+    const host = `${enrolled_ec_host}/organizations/domains/${domain}`;
+    debugLog(`Calling endpoint: [${host}]`);
+    return get(host, {
+        headers: {
+            "Host": process.env.host_header,
+            "X-Forwarded-For": ipAddress,
+            "Ocp-Apim-Subscription-Key": process.env.EXT_SUBSCRIPTION_KEY
+        }
+    })
+}
+
+function getEnrolledStationForEC(domain, creditorInstitutionCode) {
+    const host = `${enrolled_ec_host}/organizations/${creditorInstitutionCode}/domains/${domain}`;
     debugLog(`Calling endpoint: [${host}]`);
     return get(host, {
         headers: {
