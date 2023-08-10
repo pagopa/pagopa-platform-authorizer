a3c6cebcaf51e3e1000080ba10751a8bf331dce9
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-08-10 14:47:35 +0200

    [PAGOPA-1156] fix: updated integration tests

--- integration-test/src/step_definitions/support/steps.js
+++ integration-test/src/step_definitions/support/steps.js
@@ -5,11 +5,7 @@ const {
     executeAfterAllStep,
     executeAuthorizedInvocation, 
     executeHealthCheckForGPDPayments, 
-    generateAuthorization,
-    executeGetEnrolledECInvocation,
-    executeGetEnrolledStationsForECInvocation,
-    assertResponseWithEnrolledCI,
-    assertResponseWithEnrolledStations
+    generateAuthorization
 } = require('./logic/common_logic');
 
 
@@ -23,17 +19,21 @@ let bundle = {
 }
 
 /* 
- *  'Given' precondition for health checks on various services. 
+ *  'Given' preconditions
  */
 Given('GPD-Payments service running', () => executeHealthCheckForGPDPayments());
 Given('an authorization on entity {string} for the domain {string} related to subscription key "A" is added in the database', (entity, domain) => generateAuthorization(entity, domain, bundle));
+
+/* 
+ *  'When' operations
+ */
 When('the client execute a call for entity {string} with subscription key {string}', (entity, subkeyType) => executeAuthorizedInvocation(entity, subkeyType, bundle));
-When('the client execute a call for the domain {string}', (domain) => executeGetEnrolledECInvocation(domain, bundle));
-When('the client execute a call for the domain {string} for entity {creditorInstitutionCode}', (domain, creditorInstitutionCode) => executeGetEnrolledStationsForECInvocation(domain, creditorInstitutionCode, bundle));
+
+/* 
+ *  'Then' postconditions
+ */
 Then('the client receives status code {int}', (statusCode) => assertStatusCodeEquals(bundle.response, statusCode));
 Then('the client receives status code different from {int}', (statusCode) => assertStatusCodeNotEquals(bundle.response, statusCode));
-Then('the client receives an object with enrolled creditor institutions', () => assertResponseWithEnrolledCI(bundle.response));
-Then('the client receives an object with enrolled station for creditor institution', () => assertResponseWithEnrolledStations(bundle.response));
 
 
 Before(function(scenario) {
