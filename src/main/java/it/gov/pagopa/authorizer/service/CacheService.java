9f85d1df695c997542ff4ed37a9e5e7ac0503cca
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-11-23 16:33:53 +0100

    [PAGOPA-1351] fix

--- src/main/java/it/gov/pagopa/authorizer/service/CacheService.java
+++ src/main/java/it/gov/pagopa/authorizer/service/CacheService.java
@@ -64,6 +64,8 @@ public class CacheService {
 
         } catch (URISyntaxException | IOException e) {
             this.logger.log(Level.SEVERE, "An error occurred while trying to calling APIM's Authorizer API. The communication with APIM's API failed. ", e);
+        } catch (Exception e) {
+            this.logger.log(Level.SEVERE, "A generic error occurred. ", e);
         }
         return response;
     }
